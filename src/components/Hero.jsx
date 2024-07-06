007f288493db2fb460ee0895fd7fb1305e1977ab
TamimOp <tamimshadanik@gmail.com>
2024-07-06 15:19:46 +0600

    update 3

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,8 @@
+import React from "react";
 import { motion } from "framer-motion";
 import { styles } from "../styles";
 import { Suspense } from "react";
-const ComputersCanvas = React.lazy(() => import("./canvas"));
+import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
   return (
@@ -24,9 +25,8 @@ const Hero = () => {
           </p>
         </div>
       </div>
-      <Suspense fallback={<p>This is Loading...</p>}>
-        <ComputersCanvas />
-      </Suspense>
+
+      <ComputersCanvas />
 
       <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
         <a href="#about">
